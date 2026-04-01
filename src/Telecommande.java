3f9ee24833803b461b573e4a7df15645ffab2e7f
Marin Clement <clement.marin5@etu.univ-lorraine.fr>
2026-04-01 17:49:07 +0200

    Ajout du squelette de Telecommande

---
+++ src/Telecommande.java
@@ -0,0 +1,38 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Telecommande {
+
+    private List<Lampe> lampes;
+
+    // Constructeur vide
+    public Telecommande() {
+        this.lampes = new ArrayList<>();
+    }
+
+    // Ajouter une lampe
+    public void ajouterLampe(Lampe l) {
+        throw new Error("code non ecrit");
+    }
+
+    // Activer une lampe par indice
+    public void activerLampe(int indiceLampe) {
+        throw new Error("code non ecrit");
+    }
+
+    // Désactiver une lampe par indice
+    public void desactiverLampe(int indiceLampe) {
+        throw new Error("code non ecrit");
+    }
+
+    // Activer toutes les lampes
+    public void activerTout() {
+        throw new Error("code non ecrit");
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        throw new Error("code non ecrit");
+    }
+}
